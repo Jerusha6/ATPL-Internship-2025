affba2341ab6eae160462386a1f31412d3024381
jerusha <jerusharanitamiri8663@gmail.com>
2025-07-03 16:27:48 +0530

    Add Java assignment 2 Q5

---
+++ VehicleCls.java
@@ -0,0 +1,27 @@
+class Vehicle{
+void start(){
+    System.out.println("Vehicle is starting");
+}
+}
+class Bike extends Vehicle{
+    @Override
+    void start(){
+    System.out.println("Bike is starting");
+}
+}
+class Car extends Bike{
+    @Override
+    void start(){
+    System.out.println("Car is starting");
+}
+}
+class VehicleCls{
+    public static void main(String[] args){
+        Vehicle ve = new Vehicle();
+        ve.start();
+        Bike bike = new Bike();
+        bike.start();
+        Car car = new Car();
+        car.start();
+    }
+}
