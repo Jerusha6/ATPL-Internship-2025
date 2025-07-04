17dd72eeba4abbdde31d16256829f2ce0190343d
jerusha <jerusharanitamiri8663@gmail.com>
2025-07-04 17:31:02 +0530

    organize assignments

---
+++ java-assignment-2/AbstractShape.java
@@ -0,0 +1,13 @@
+abstract class Shape{
+    abstract void draw();
+}
+class AbstractShape{
+    public static void main(String[] args){
+        Shape sh = new Shape() {
+            void draw(){
+                System.out.println("Circle shape is being draw");
+            }
+        };
+        sh.draw();
+    }
+}
