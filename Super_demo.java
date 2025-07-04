30bdc426654c9731ff29a243af8e0f952a5fab0e
jerusha <jerusharanitamiri8663@gmail.com>
2025-07-04 08:23:25 +0530

    java assignment 2 Q20

---
+++ Super_demo.java
@@ -0,0 +1,18 @@
+class baseClass {
+void message() {
+System.out.println("This is base class");
+}
+}
+
+class definedClass extends baseClass {
+void message() {
+super.message();
+System.out.println("This is defined class");
+}
+}
+public class Super_demo {
+public static void main(String args[]) {
+definedClass dc = new definedClass();
+dc.message();
+}
+}
