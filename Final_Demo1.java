c817e0bbc897c85a499d2c26db745b303bbdc94a
jerusha <jerusharanitamiri8663@gmail.com>
2025-07-04 11:33:47 +0530

    java assignment 2 Q25

--- Final_Demo1.java
+++ Final_Demo1.java
@@ -1,12 +1,12 @@
 class DemoCls1{
     final void PI_value(){
-        double PI=3.14;
+        double PI_value=3.14;
     }
 }
 class DemoCls2 extends DemoCls1{
     @Override
     void PI_value(){
-        PI = 3.01;
+        PI_value = 3.01;
     }
 }
 class Final_Demo1 {
