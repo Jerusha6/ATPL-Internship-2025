78b795ec5a771a3409a8178f837d3628962c1b00
jerusha <jerusharanitamiri8663@gmail.com>
2025-07-04 08:27:49 +0530

    java assignment 2 Q21

---
+++ This_demo.java
@@ -0,0 +1,17 @@
+class Demo{
+    String name;
+    int age;
+    Demo(String name, int age){
+        this.name=name;
+        this.age=age;
+    }
+    void showMsg(){
+        System.out.println("Name: "+name+" ,age: "+age);
+    }
+}
+class This_demo{
+    public static void main(String[] args) {
+        Demo demo = new Demo("Jerusha", 20);
+        demo.showMsg(); 
+    }
+}
