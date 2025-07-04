8ca86eecd03814331fea4e149a94136afe718c75
jerusha <jerusharanitamiri8663@gmail.com>
2025-07-04 10:30:17 +0530

    java assignment 2 Q22

---
+++ This_method.java
@@ -0,0 +1,17 @@
+class Class1{
+    Class1(){
+        System.out.println("Defualt Constructor");
+    }
+    Class1(int a){
+        this();
+        System.out.println("Parameterized constructor");
+
+    }
+
+}
+
+class This_method {
+    public static void main(String[] args){
+        Class1 ob = new Class1(5);
+    }
+}
