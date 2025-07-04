5b04c88a00bafaff793e79e593d6de79c3b565e0
Jerusha <jerusharanitamiri8663@gmail.com>
2025-07-04 06:56:47 +0530

    add java assignement 2 Q18

---
+++ CounterCls.java
@@ -0,0 +1,19 @@
+class Counter {
+    static int count=0;
+    Counter(){
+    count++;
+    }
+    void display(){
+        System.out.println("Object count "+count);
+    }
+}
+
+public class CounterCls {
+    public static void main(String[] args) {
+       Counter co1 = new Counter();
+       Counter co2 = new Counter();
+       Counter co3 = new Counter();
+       Counter co4 = new Counter();
+       co4.display();
+    }
+}
