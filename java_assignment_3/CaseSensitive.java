db20359c6378cb46354e2f6ddc87f27318575546
Jerusha <jerusharanitamiri8663@gmail.com>
2025-07-05 13:26:15 +0530

    Case sensitive demo

---
+++ java_assignment_3/CaseSensitive.java
@@ -0,0 +1,11 @@
+public class CaseSensitive {
+    public static void main(String[] args) {
+        String str1 = "Java";
+        String str2 = "java";
+        System.out.println("str1: "+str1);
+        System.out.println("str2: "+str2);
+        System.out.println("str1.equals(str2): "+str1.equals(str2));
+        System.out.println("str1.equalsIgnoreCase(str2): "+str1.equalsIgnoreCase(str2));
+        System.out.println("str1.compareTo(str2): "+str1.compareTo(str2));
+    }
+}
