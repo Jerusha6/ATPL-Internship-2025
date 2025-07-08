72f6d7a4909e3a17a8f7de4a09ec5bb436bae9fc
jerusha <jerusharanitamiri8663@gmail.com>
2025-07-08 14:14:41 +0530

    stack collection framework

--- java_assignment_5/Stackcls.java
+++ java_assignment_5/Stackcls.java
@@ -3,16 +3,18 @@ import java.util.Scanner;
 class Stackcls {
 public static void main(String[] args){
     Stack<Integer> st = new Stack<>();
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Enter any 5 elements to push into stack: ");
-    for(int i=0;i<5;i++){
-        st.push(sc.nextInt());
+    
+    try(Scanner sc = new Scanner(System.in)){
+        System.out.println("Enter any 5 elements to push into stack: ");
+        for(int i=0;i<5;i++){
+            st.push(sc.nextInt());
+        }
+        System.out.println("Here are the five elements you pushed into stack: ");
+        System.out.println("Stack: " + st); 
+        st.pop();
+        st.pop();
+        System.out.println("\nHere is the updated list after applying pop operation to remove 2 elements from the stack: ");
+        System.out.println("Stack: " + st);  
     }
-    System.out.println("Here are the five elements you pushed into stack: ");
-    System.out.println("Stack: " + st); 
-    st.pop();
-    st.pop();
-    System.out.println("\nHere is the updated list after applying pop operation to remove 2 elements from the stack: ");
-    System.out.println("Stack: " + st);  
 }
 }
