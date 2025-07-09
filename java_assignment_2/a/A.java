fd67e72a2ea9770d474c13251249b5e9f4885d73
Jerusha <jerusharanitamiri8663@gmail.com>
2025-07-09 21:39:55 +0530

    assignments

---
+++ java_assignment_2/a/A.java
@@ -0,0 +1,7 @@
+package a;
+
+public class A {
+    public void sayHello() {
+        System.out.println("Hello from a and class B!");
+    }
+}
---
+++ java_assignment_2/b/B.java
@@ -0,0 +1,9 @@
+package b;
+import a.A;
+
+public class B {
+    public static void main(String[] args) {
+        B obj = new B();
+        obj.sayHello();
+    }
+}
