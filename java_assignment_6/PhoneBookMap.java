3c6ca036aa88e32b8ea8590d0fd92122d16a47bd
jerusha <jerusharanitamiri8663@gmail.com>
2025-07-10 08:59:46 +0530

    assignments

--- java_assignment_6/PhoneBookMap.java
+++ java_assignment_6/PhoneBookMap.java
@@ -6,7 +6,7 @@
 // ● Search contact
 // ● Remove contact
 
-// package com.aaslin.java.assignments.assignment6;
+package com.aaslin.java.assignments.assignment6;
 import java.util.Scanner;
 import java.util.Map;
 import java.util.HashMap;
@@ -21,6 +21,14 @@ class PhoneBook {
             System.out.println(name+"->"+map.get(name)+" removed.");
             map.remove(name,map.get(name));
         }
+        else if(map.containsValue(name)) {
+        	for(Map.Entry<String, String> entry:map.entrySet()) {
+        		if((entry.getValue()).equals(name)) {
+        			System.out.println(entry.getKey()+"-"+name+" removed");
+                    map.remove(entry.getKey(),name);
+        		}
+        	}
+        }
         else{
             System.out.println("There is no such contact with "+name);
         }
@@ -29,16 +37,22 @@ class PhoneBook {
         if(map.containsKey(name)){
             System.out.println(name+"->"+map.get(name));   
         }
+        else if(map.containsValue(name)) {
+        	for(Map.Entry<String, String> entry:map.entrySet()) {
+        		if((entry.getValue()).equals(name)) {
+        			System.out.println(entry.getKey()+"-"+name);
+        		}
+        	}
+        }
         else{
             System.out.println("There is no such contact with "+name);
         }
            
     }
     public void getContacts(){
-        System.out.println("Key->Value");
         if(map.size()!=0){
             for(Map.Entry<String, String> entry : map.entrySet()) {
-                System.out.println(entry.getKey()+"->"+entry.getValue());
+                System.out.println(entry.getKey()+"-"+entry.getValue());
                 }
         }
         else{
@@ -48,18 +62,58 @@ class PhoneBook {
     }
 }
 class PhoneBookMap{
-public static void main(String args[]) {
-    PhoneBook phoneBook = new PhoneBook();
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("Enter your name: ");
-    String name = scanner.nextLine();
-    System.out.println("Enter your mobile number: ");
-    String mobileNumber = scanner.nextLine();
-    phoneBook.addContact(name,mobileNumber);
-    phoneBook.removeContact(name);
-    System.out.println("Enter name to search: ");
-    phoneBook.searchContact(scanner.nextLine());
-    phoneBook.getContacts();
-    scanner.close();
-}
+	public static void main(String args[]) {
+	    PhoneBook phoneBook = new PhoneBook();
+	    Scanner scanner = new Scanner(System.in);
+	    boolean exit=true;
+	    int choice=0;
+	    while(exit){
+		    System.out.println("Enter your choice: ");
+		    System.out.println("1. Add contact\n2. Delete contact\n3. Search contact\n4. Show contacts\n5. Exit");
+		    choice = scanner.nextInt();
+		    scanner.nextLine();
+		    switch(choice){
+		        case 1: {
+		        	System.out.println("Enter name: ");
+		            String name = scanner.nextLine();
+		            System.out.println("Enter mobile number: ");
+		            String mobileNumber = scanner.nextLine();
+		            if(mobileNumber.length()==10) {		            	
+		            	phoneBook.addContact(name,mobileNumber);
+		            }
+		            else {
+		            	while(mobileNumber.length()!=10) {
+		            		System.out.println("Number should be 10 digits, try again!");
+		            		mobileNumber = scanner.nextLine();
+		            	}
+		            	phoneBook.addContact(name,mobileNumber);
+		            }
+		            break;
+		        }
+		        case 2: {
+		        	System.out.println("Enter contact name/Mobile number to remove from contact list: ");
+		        	phoneBook.removeContact(scanner.nextLine());
+		            break;
+		        }
+		        case 3:{
+		        	System.out.println("Enter name/Mobile number to search: ");
+		            phoneBook.searchContact(scanner.nextLine());
+		            break;
+		        }
+		        case 4:{
+		        	phoneBook.getContacts();
+		            break;
+		        }
+		        case 5:{
+		            exit=false;
+		            break;
+		        }
+		        default:{
+		            System.out.println("Please enter a valid number..");
+		            break;
+		        }
+		    } 
+	    }
+	    scanner.close();
+	}
 }
