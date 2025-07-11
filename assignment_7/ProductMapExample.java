b328c458fcd41f14ef99682d4b7a5d75833b2af0
Jerusha <jerusharanitamiri8663@gmail.com>
2025-07-11 22:47:29 +0530

    assignment

---
+++ assignment_7/ProductMapExample.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class ProductMapExample {
+    public static void main(String[] args) {
+        // Create the Map of product names and their prices
+        Map<String, Integer> productPrices = new HashMap<>();
+        productPrices.put("Laptop", 50000);
+        productPrices.put("Mobile", 20000);
+        productPrices.put("Headphones", 1500);
+        productPrices.put("Keyboard", 800);
+
+        // Get the entry set and create an iterator
+        Iterator<Map.Entry<String, Integer>> iterator = productPrices.entrySet().iterator();
+
+        // Iterate and print key-value pairs
+        while (iterator.hasNext()) {
+            Map.Entry<String, Integer> entry = iterator.next();
+            System.out.println("Product: " + entry.getKey() + ", Price: " + entry.getValue());
+        }
+    }
+}
