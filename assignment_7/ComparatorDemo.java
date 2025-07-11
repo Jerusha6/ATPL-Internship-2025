32f1ed9f5446c6630379395399397c25e2a99add
Jerusha <jerusharanitamiri8663@gmail.com>
2025-07-11 22:29:45 +0530

    assignment

--- assignment_7/ComparatorDemo.java
+++ assignment_7/ComparatorDemo.java
@@ -1,4 +1,4 @@
-package com.aaslin.java.assignments.assignment_7;
+// package com.aaslin.java.assignments.assignment_7;
 import java.util.Collections;
 import java.util.List;
 import java.util.ArrayList;
@@ -32,19 +32,20 @@ class Student{
     }
 
     public String toString() {
-        return "name: "+name + " , marks: " + marks + " , age: " + age + " , id: " + id;
+        return "name: "+name + " | marks: " + marks + " | age: " + age + " | id: " + id;
     }
 }
 
 class StudentComparator implements Comparator<Student> {
     public int compare(Student s1, Student s2) {
-        // Compare by name (A-Z)
-        int nameResult = s1.getName().compareTo(s2.getName());
-        if (nameResult != 0) return nameResult;
 
         // Compare by marks (Descending)
         int marksResult = Double.compare(s2.getMarks(), s1.getMarks());
         if (marksResult != 0) return marksResult;
+
+        // Compare by name (A-Z)
+        int nameResult = s1.getName().compareTo(s2.getName());
+        if (nameResult != 0) return nameResult;
 
         // Compare by age (Ascending)
         return s1.getAge() - s2.getAge();
@@ -64,8 +65,33 @@ class ComparatorDemo {
         studentList.add(new Student(106, "Roja", 36, 100));
         studentList.add(new Student(107, "Anand", 41, 100));
 
+        System.out.println("Using StudentComparator() class");
         // Sorting using the comparator
         Collections.sort(studentList, new StudentComparator());
+        
+        System.out.println("Using Comparator chaining");
+        // Using Comparator chaining
+        Comparator<Student> chainComparator = Comparator.comparingDouble(Student :: getMarks).reversed().thenComparing(Student::getName).thenComparing(Student::getAge);
+        Collections.sort(studentList, chainComparator);
+        
+        // Using ananymous Comparator
+        
+        System.out.println("Using ananymous Comparator");
+        Collections.sort(studentList, new Comparator<Student>() {
+            public int compare(Student s1, Student s2) {
+                // Marks descending
+                int result = Double.compare(s2.getMarks(), s1.getMarks());
+                if (result != 0) return result;
+
+                // Name ascending
+                result = s1.getName().compareTo(s2.getName());
+                if (result != 0) return result;
+
+                // Age ascending
+                return s1.getAge() - s2.getAge();
+            }
+        });
+
 
         // Print the sorted list
         for (Student s : studentList) {
