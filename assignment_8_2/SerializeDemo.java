9d25348f0e56936e119db56667907627be9a7d3c
jerusha <jerusharanitamiri8663@gmail.com>
2025-07-16 10:22:08 +0530

    assignments

--- assignment_8_2/SerializeDemo.java
+++ assignment_8_2/SerializeDemo.java
@@ -1,34 +1,79 @@
- package com.aaslin.java.assignments.assignment_8_2;
+// package com.aaslin.java.assignments.assignment_8_2;
 
-import com.aaslin.java.assignments.assignment_8_2.Student;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.io.Serializable;
+
+class Student implements Serializable{
+
+	private static final long serialVersionUID = 232456744L;
+    private int id;
+    private String name;
+    private int age;
+    private String department; 
+    private double gpa;
+
+	public Student(int id, String name, int age, String department, double gpa) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.department = department;
+		this.gpa = gpa;
+	}
+    
+	public String toString(){
+		return id+" "+name+" "+age+" "+department+" "+gpa;
+	}
+
+}
 
 public class SerializeDemo {
     public static void main(String args[]){
 
-    	List<Student> list = new ArrayList<>();
-		list.add(new Student(1, "JERUSHA", 21, "CSE", 8));
-		list.add(new Student(2, "ELISHA", 19, "CSE", 8));
-		list.add(new Student(3, "ANAND", 41, "CSE", 8));
-		list.add(new Student(4, "ROJA", 31, "CSE", 8));
-		list.add(new Student(5, "BHARANI", 20, "CSE", 8));
+    	List<Student> studentList = new ArrayList<>();
+		studentList.add(new Student(1, "JERUSHA", 21, "CSE", 8));
+		studentList.add(new Student(2, "ELISHA", 19, "CSE", 8));
+		studentList.add(new Student(3, "ANAND", 41, "CSE", 8));
+		studentList.add(new Student(4, "ROJA", 31, "CSE", 8));
+		studentList.add(new Student(5, "BHARANI", 20, "CSE", 8));
+		File fileName = new File("students.ser");
     	try {
-    		
-    		File fileName = new File("students.ser");
         	FileOutputStream file = new FileOutputStream(fileName);
         	ObjectOutputStream out = new ObjectOutputStream(file);
         	        	
-        	out.writeObject(list);
+        	out.writeObject(studentList);
+
+			file.close();
+			out.close();
  	
     	}catch(IOException io) {
     		System.out.print(io.getMessage());
     	}
+
+		studentList = null; //dereferencing the object to null
     	
-    	
+    	try{
+			FileInputStream file = new FileInputStream(fileName);
+			ObjectInputStream in = new ObjectInputStream(file);
+			
+			studentList = (List) in.readObject();
+			in.close();
+			file.close();
+
+			for(Student student : studentList){
+				System.out.println(student);
+			}
+		}catch(IOException io){
+			System.out.println(io.getMessage());
+		}catch(ClassNotFoundException cne){
+			System.out.println(cne.getMessage());
+		}
     }
 }
