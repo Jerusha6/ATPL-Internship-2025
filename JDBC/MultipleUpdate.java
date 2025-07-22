3cd76c712f15aa30c038c53e4119d67bdd992939
Jerusha <jerusharanitamiri8663@gmail.com>
2025-07-22 23:00:19 +0530

    JDBC assignments

--- JDBC/MultipleUpdate.java
+++ JDBC/MultipleUpdate.java
@@ -1,24 +1,25 @@
 import java.sql.*;
 
-class PartialMismatch {
+class MultipleUpdate {
     public static void main(String[] args){
         String url = "jdbc:mysql://192.168.71.15:3306/intern";
 
         String username = "intern2025";
         String password = "intern2025";
 
-        String query = " SELECT * FROM products WHERE name LIKE ? ";
+        String query = " Update users_jerusha set email = ? , contact = ? where user_id = 101";
         try{
             Connection conn = DriverManager.getConnection(url, username, password);
             PreparedStatement preparedStatement = conn.prepareStatement(query);
-            preparedStatement.setString(1, "%phone%");
+            preparedStatement.setString(1, "jerusharani123@gmail.com");
+            preparedStatement.setInt(2, Integer.parseInt("9565676788"));
 
             ResultSet rs = preparedStatement.executeQuery(query);
             while(rs.next()){
-                System.out.print(rs.getString(1)+" ");
+                System.out.print(rs.getInt(1)+" ");
                 System.out.print(rs.getString(2)+" ");
-                System.out.print(rs.getDate(3)+" ");
-                System.out.print(rs.getDouble(4)+" ");
+                System.out.print(rs.getString(3)+" ");
+                System.out.print(rs.getInt(4)+" ");
                 System.out.println();
             }
             conn.close();
