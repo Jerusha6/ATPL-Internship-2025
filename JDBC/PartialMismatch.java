96133345a9679d4f36a579ea55059859a2dcdc3d
Jerusha <jerusharanitamiri8663@gmail.com>
2025-07-22 22:45:22 +0530

    JDBC assignments

---
+++ JDBC/PartialMismatch.java
@@ -0,0 +1,32 @@
+import java.sql.*;
+
+class PartialMismatch {
+    public static void main(String[] args){
+        String url = "jdbc:mysql://192.168.71.15:3306/intern";
+
+        String username = "intern2025";
+        String password = "intern2025";
+
+        String query = " SELECT * FROM products WHERE name LIKE ? ";
+        try{
+            Connection conn = DriverManager.getConnection(url, username, password);
+            PreparedStatement preparedStatement = conn.prepareStatement(query);
+            preparedStatement.setString(1, "%phone%");
+
+            ResultSet rs = preparedStatement.executeQuery(query);
+            while(rs.next()){
+                System.out.print(rs.getString(1)+" ");
+                System.out.print(rs.getString(2)+" ");
+                System.out.print(rs.getDate(3)+" ");
+                System.out.print(rs.getDouble(4)+" ");
+                System.out.println();
+            }
+            conn.close();
+            preparedStatement.close();
+            rs.close();
+
+        } catch (SQLException e) {
+            System.out.println("error "+e.getMessage());
+        }
+    }
+}
