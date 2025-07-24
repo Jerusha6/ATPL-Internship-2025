49461a4285541bdc014ad9bfd44212442dd23631
jerusha <jerusharanitamiri8663@gmail.com>
2025-07-24 09:58:02 +0530

    Squaring Elements in list Using Lambda and Streams

--- assignment_11/SquareElementUsingLambda.java
+++ assignment_11/SquareElementUsingLambda.java
@@ -6,13 +6,19 @@ public class SquareElementUsingLambda {
     
     public static void main(String[] args){
 
-        List<Integer> integerList = Arrays.asList(1,2,3,4,5);
+        List<Integer> integerList = Arrays.asList(6,2,4,7,3);
 
         System.out.println("List before squaring: "+integerList);
 
-        System.out.println("List after squaring: ");
+        System.out.println("List after squaring with lambda expression: ");
 
-        integerList.forEach(num -> System.err.print(num*num+" "));
+        integerList.forEach(num -> System.out.print(num*num+" "));
+
+        System.out.println();
+
+        System.out.println("List after squaring with lambda expression(with streams) also sorting in the same line: ");
+
+        integerList .stream() .sorted() .map(n->n*n) .forEach(n-> System.out.print(n +" "));
 
 
     }
