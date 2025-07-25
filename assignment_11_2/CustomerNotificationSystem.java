4c9c83b48c37d2340a6a3f5d81fd7d6f6e3e6264
jerusha <jerusharanitamiri8663@gmail.com>
2025-07-25 11:19:48 +0530

    Class name change

--- assignment_11_2/CustomerNotificationSystem.java
+++ assignment_11_2/CustomerNotificationSystem.java
@@ -2,13 +2,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 
-class customer {
+class Customer {
     String id;
     String name;
     double salary;
     boolean subscriptionStatus;
 
-    public customer(String customer_id, String customer_name, boolean subscriptionStatus) {
+    public Customer(String customer_id, String customer_name, boolean subscriptionStatus) {
         id = customer_id;
         name = customer_name;
         this.subscriptionStatus = subscriptionStatus;
@@ -19,12 +19,12 @@ class customer {
 public class CustomerNotificationSystem {
     public static void main(String[] args) {
 
-        List<customer> list = new ArrayList<>();
-        list.add(new customer("C101", "Jerusha", true));
-        list.add(new customer("C102", "Rani", false));
-        list.add(new customer("C103", "Elisha", true));
-        list.add(new customer("C104", "Anand", false));
-        list.add(new customer("C105", "Roja", true));
+        List<Customer> list = new ArrayList<>();
+        list.add(new Customer("C101", "Jerusha", true));
+        list.add(new Customer("C102", "Rani", false));
+        list.add(new Customer("C103", "Elisha", true));
+        list.add(new Customer("C104", "Anand", false));
+        list.add(new Customer("C105", "Roja", true));
 
         Consumer<String> consumer = s -> System.out.println(s);
 
