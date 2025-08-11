ca774d8befb21848d69b0601c029112c98228f4a
jerusha <jerusharanitamiri8663@gmail.com>
2025-08-11 14:41:15 +0530

    spring framework

---
+++ src/main/java/com/aaslin/BeanLifeCycle/TeacherThroughXml.java
@@ -0,0 +1,10 @@
+package com.aaslin.BeanLifeCycle;
+
+	public class TeacherThroughXml  {
+	public void init() {
+		System.out.println("\nMethod initialized through xml");	
+	}
+	public void destroy() {
+		System.out.println("\nMethod destroyed through xml");
+	}
+}
