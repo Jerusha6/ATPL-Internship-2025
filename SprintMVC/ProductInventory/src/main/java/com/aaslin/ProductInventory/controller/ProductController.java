3ea9ed1cdeda6143f145121c58886fe844c6dfc2
jerusha <jerusharanitamiri8663@gmail.com>
2025-08-13 11:25:06 +0530

    Product inventory

---
+++ SprintMVC/ProductInventory/src/main/java/com/aaslin/ProductInventory/controller/ProductController.java
@@ -0,0 +1,58 @@
+package com.aaslin.ProductInventory.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.aaslin.ProductInventory.model.Product;
+import com.aaslin.ProductInventory.service.ProductService;
+
+
+@RestController 
+@RequestMapping("/api")
+public class ProductController {
+
+	@Autowired
+	ProductService service;
+	
+	@GetMapping("/products")
+	public List<Product> show(){
+		return service.getProducts();
+	}
+	
+	@GetMapping("/products/{productId}")
+	public Product getProduct(@PathVariable int productId) {
+		return service.getProduct(productId);
+	}
+	
+	@PostMapping("/products")
+	public void addProduct(@RequestBody Product product) {
+		 service.addProduct(product);
+	}
+	
+	@PutMapping("/products/{productId}")
+	public void updateProduct(@PathVariable int productId, @RequestBody Product product) {
+		service.updateProduct(productId, product);
+	}
+	
+	//	To delete all the products
+	@DeleteMapping("/products")
+	public void deleteProducts() {
+		service.deleteProducts();
+	}
+	
+	// To delete by Id
+	@DeleteMapping("/products/{productId}")
+	public void deleteProductById(@PathVariable int productId) {
+		service.deleteProductById(productId);
+	}
+	
+}
---
+++ SprintMVC/ProductInventory/src/main/java/com/aaslin/ProductInventory/service/ProductService.java
@@ -0,0 +1,66 @@
+package com.aaslin.ProductInventory.service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.aaslin.ProductInventory.model.Product;
+
+
+@Service 
+public class ProductService {
+	
+	@Autowired 
+	Product product;
+	List<Product> productList = new ArrayList<>(Arrays.asList(
+					new Product(1, "ThinkCentre", 79000.00),
+					new Product(2,"Daikin AC", 44999.00),
+					new Product(3,"AC REMOTE", 1000.00)
+			));
+	
+	// To fetch all products
+	public List<Product> getProducts() {
+		return productList;
+	}
+	
+	// To fetch single product by ID
+	public Product getProduct(int productId) {
+	    return productList.stream()
+	                   .filter(p -> p.getProductId() == productId)
+	                   .findFirst()
+	                   .orElse(null);
+	}
+	
+	// To add product into the list
+	public void addProduct(Product product) {
+		productList.add(product);
+	}
+
+	// To update product 
+	public void updateProduct(int productId, Product updateProduct) {
+		 for(int i=0;i<productList.size();i++) {
+			 if(productList.get(i).getProductId()==productId) {
+				 productList.set(i, updateProduct);
+				 break;
+			 }
+		 }
+	}
+	
+	// To delete all products
+	public void deleteProducts() {
+		productList.removeAll(productList);
+	}
+	
+	// To delete product by ID
+	public void deleteProductById(int productId) {
+		 for(int i=0;i<productList.size();i++) {
+			 if(productList.get(i).getProductId()==productId) {
+				 productList.remove(i);
+				 break;
+			 }
+		 }
+	}
+}
