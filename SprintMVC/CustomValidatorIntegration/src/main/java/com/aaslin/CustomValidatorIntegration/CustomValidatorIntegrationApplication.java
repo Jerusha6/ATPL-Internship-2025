973a616bbdf6ead6d8f4712e5374b6c7bebdf910
jerusha <jerusharanitamiri8663@gmail.com>
2025-08-13 17:03:32 +0530

    Custom Validator Integration

---
+++ SprintMVC/CustomValidatorIntegration/src/main/java/com/aaslin/CustomValidatorIntegration/CustomValidatorIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.aaslin.CustomValidatorIntegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomValidatorIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomValidatorIntegrationApplication.class, args);
+	}
+
+}
